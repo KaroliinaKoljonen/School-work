e783d2b3d8265ac43c963722662b371488a33005
KaroliinaKoljonen <p.karoliina.koljonen@gmail.com>
2024-11-04 19:09:30 +0000

    Add files via upload

---
+++ .vs/Program.cs
@@ -0,0 +1,13 @@
+﻿using AppLogger;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Log("this_is_a_sample_text");
+        }
+    }
+}
+
